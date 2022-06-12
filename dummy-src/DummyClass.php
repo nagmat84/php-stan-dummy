c3c9cbd3bb7216d75d4cf0a0225c90e1c1b688c9
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-06-12 18:08:28 +0200

    Modified dummy tests.

--- dummy-src/DummyClass.php
+++ dummy-src/DummyClass.php
@@ -2,6 +2,18 @@
 
 namespace Nagmat84\PhpStanDummy;
 
-function foo(int $a, int $b): bool {
-	return $a === $b;
+function foo(int $a): bool {
+	return boolval($a);
+}
+
+function bar(int $a): bool {
+	return (bool)$a;
+}
+
+function fooBar(int $a, int $b): bool {
+	return $a == $b;
+}
+
+function barFoo(int $a, int $b): bool {
+	return $a != $b;
 }
